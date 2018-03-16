38fd936890e49f9f45cbfb31d3e5a15b3577b5a4
Bastian Brinkmann <babri@mail.uni-paderborn.de>
2018-03-16 12:33:02 +0100

    Missing project

---
+++ RTSFramework/RTSFramework.Concrete.User/UserIntendedChangesDiscoverer.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using RTSFramework.Contracts;
+using RTSFramework.Contracts.Artefacts;
+using RTSFramework.Contracts.Delta;
+using RTSFramework.Core.Artefacts;
+
+namespace RTSFramework.Concrete.User
+{
+    //TODO: This is a delta provider not a discoverer
+    public class UserIntendedChangesDiscoverer<TP> : IOfflineDeltaDiscoverer<TP, StructuralDelta<FileElement>> where TP : IProgramModel
+    {
+        private readonly List<FileElement> changedFiles;
+
+        public UserIntendedChangesDiscoverer(IEnumerable<string> filePaths)
+        {
+            changedFiles = filePaths.Select(x => new FileElement(x)).ToList();
+        }
+
+        public StructuralDelta<FileElement> Discover(TP oldVersion, TP newVersion)
+        {
+            var delta = new StructuralDelta<FileElement>
+            {
+                SourceModelId = oldVersion.VersionId,
+                TargetModelId = newVersion.VersionId
+            };
+
+            delta.ChangedElements.AddRange(changedFiles);
+
+            return delta;
+        }
+    }
+}
