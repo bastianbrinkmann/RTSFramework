bac2dcccb2f2155773b8302a171cc18dfa60f876
Bastian Brinkmann <babri@mail.uni-paderborn.de>
2018-04-12 15:02:39 +0200

    Missing files

---
+++ RTSFramework/RTSFramework.Concrete.User/IntendedFileChangesProvider.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace RTSFramework.Concrete.User
+{
+	public class IntendedFileChangesProvider : IIntendedChangesProvider
+	{
+		public IList<string> IntendedChanges { get; set; } = new List<string>();
+	}
+}
