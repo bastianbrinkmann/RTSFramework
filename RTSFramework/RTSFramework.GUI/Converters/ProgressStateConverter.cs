783a93ed849f897315bc511ee8623fc5a892772b
Bastian Brinkmann <babri@mail.uni-paderborn.de>
2018-04-22 09:51:03 +0200

    Task Item indicating run process

---
+++ RTSFramework/RTSFramework.GUI/Converters/ProgressStateConverter.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Data;
+using System.Windows.Markup;
+using System.Windows.Shell;
+
+namespace RTSFramework.GUI.Converters
+{
+	public class ProgressStateConverter : MarkupExtension, IValueConverter
+	{
+		private static ProgressStateConverter progressStateConverter;
+		public override object ProvideValue(IServiceProvider serviceProvider)
+		{
+			return progressStateConverter ?? (progressStateConverter = new ProgressStateConverter());
+		}
+
+		public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+		{
+			bool isRunning = value != null && (bool)value;
+			return isRunning ? TaskbarItemProgressState.Indeterminate : TaskbarItemProgressState.None;
+		}
+
+		public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
