f6c5430775bf605e6f2a41e5bfe2c7a6c6609a32
Bastian Brinkmann <babri@mail.uni-paderborn.de>
2018-05-09 12:44:59 +0200

    Missing ignored files

---
+++ RTSFramework/RTSFramework.Concrete.User/IntendedChangesAdapter.cs
@@ -0,0 +1,38 @@
+﻿using System.IO;
+using RTSFramework.Concrete.User.Models;
+using RTSFramework.Contracts.Adapter;
+using RTSFramework.Contracts.Models.Delta;
+using RTSFramework.Core.Models;
+using RTSFramework.Core.Utilities;
+
+namespace RTSFramework.Concrete.User
+{
+	public class IntendedChangesAdapter<TDelta> : IArtefactAdapter<IntendedChangesArtefact, TDelta> 
+		where TDelta : IDelta<LocalProgramModel>
+	{
+		private readonly IDeltaAdapter<StructuralDelta<LocalProgramModel, FileElement>, TDelta, LocalProgramModel> deltaAdapter;
+
+		public IntendedChangesAdapter(IDeltaAdapter<StructuralDelta<LocalProgramModel, FileElement>, TDelta, LocalProgramModel> deltaAdapter)
+		{
+			this.deltaAdapter = deltaAdapter;
+		}
+
+		public TDelta Parse(IntendedChangesArtefact artefact)
+		{
+			var delta = new StructuralDelta<LocalProgramModel, FileElement>(artefact.LocalProgramModel, artefact.LocalProgramModel);
+
+			foreach (string intendedChange in artefact.IntendedChanges)
+			{
+				var relativePathToSolution = RelativePathHelper.GetRelativePath(artefact.LocalProgramModel, intendedChange);
+				delta.ChangedElements.Add(new FileElement(relativePathToSolution, () => File.ReadAllText(intendedChange)));
+			}
+
+			return deltaAdapter.Convert(delta);
+		}
+
+		public void Unparse(TDelta model, IntendedChangesArtefact artefact)
+		{
+			throw new System.NotImplementedException();
+		}
+	}
+}
---
+++ RTSFramework/RTSFramework.Concrete.User/IntendedChangesArtefact.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using RTSFramework.Concrete.User.Models;
+
+namespace RTSFramework.Concrete.User
+{
+	public class IntendedChangesArtefact
+	{
+		public IList<string> IntendedChanges { get; set; } = new List<string>();
+
+		public LocalProgramModel LocalProgramModel { get; set; }
+	}
+}
