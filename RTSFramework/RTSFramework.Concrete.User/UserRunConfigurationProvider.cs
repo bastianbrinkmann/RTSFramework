241ab4df9cbcb052b49e1fd7347b366d00e4809b
Bastian Brinkmann <babri@mail.uni-paderborn.de>
2018-05-14 15:06:17 +0200

    Missing file in ignored folder

---
+++ RTSFramework/RTSFramework.Concrete.User/UserRunConfigurationProvider.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using RTSFramework.Contracts.Utilities;
+
+namespace RTSFramework.Concrete.User
+{
+	public class UserRunConfigurationProvider : IUserRunConfigurationProvider
+	{
+		public IList<string> IntendedChanges { get; set; } = new List<string>();
+
+		public double TimeLimit { get; set; }
+	}
+}
