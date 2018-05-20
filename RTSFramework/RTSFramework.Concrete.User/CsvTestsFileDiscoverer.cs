ac326f6840561ac74cc6754bb4bd43f69ebd99ff
Bastian Brinkmann <babri@mail.uni-paderborn.de>
2018-05-20 16:06:49 +0200

    Ignored files

---
+++ RTSFramework/RTSFramework.Concrete.User/CsvTestsFileDiscoverer.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
+using RTSFramework.Concrete.User.Models;
+using RTSFramework.Contracts;
+using RTSFramework.Contracts.Models;
+using RTSFramework.Contracts.Utilities;
+
+namespace RTSFramework.Concrete.User
+{
+	public class CsvTestsFileDiscoverer<TModel> : ITestsDiscoverer<TModel, CsvFileTestcase> where TModel : IProgramModel
+	{
+		private readonly IUserRunConfigurationProvider runConfigurationProvider;
+
+		public CsvTestsFileDiscoverer(IUserRunConfigurationProvider runConfigurationProvider)
+		{
+			this.runConfigurationProvider = runConfigurationProvider;
+		}
+
+		public Task<IList<CsvFileTestcase>> GetTestCasesForModel(TModel model, CancellationToken token)
+		{
+			var csvFile = runConfigurationProvider.CsvTestsFile;
+			if (!File.Exists(csvFile))
+			{
+				throw new ArgumentException($"The CSV file '{csvFile}' does not exist!");
+			}
+
+			IList<CsvFileTestcase> tests = new List<CsvFileTestcase>();
+
+			foreach (string line in File.ReadAllLines(csvFile))
+			{
+				token.ThrowIfCancellationRequested();
+
+				string testName = line.Substring(0, line.IndexOf(';'));
+				string linkedClass = line.Substring(line.IndexOf(';') + 1);
+
+				tests.Add(new CsvFileTestcase
+				{
+					Id = testName,
+					AssociatedClass = linkedClass
+				});
+			}
+
+			return Task.FromResult(tests);
+		}
+	}
+}
---
+++ RTSFramework/RTSFramework.Concrete.User/Models/CsvFileTestcase.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using RTSFramework.Contracts.Models;
+
+namespace RTSFramework.Concrete.User.Models
+{
+	public class CsvFileTestcase : ITestCase
+	{
+		public string Id { get; set; }
+		public List<string> Categories => new List<string>();
+		public string Name => Id;
+		public string AssociatedClass { get; set; }
+		public bool IsChildTestCase => false;
+		public Func<IList<string>> GetResponsibleChangesForLastImpact { get; set; }
+	}
+}
